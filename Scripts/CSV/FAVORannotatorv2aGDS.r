b117472e8441a6349922dd36d26e3317e2458f1b
Hufeng Zhou <zhouhufeng@holylogin03.rc.fas.harvard.edu>
2022-09-12 17:25:09 -0400

    csv

--- Scripts/CSV/FAVORannotatorv2aGDS.r
+++ Scripts/CSV/FAVORannotatorv2aGDS.r
@@ -3,18 +3,20 @@ gc()
 ### R package
 library(gdsfmt)
 library(SeqArray)
-library(SeqVarTools)
 library(readr)
+source('config.R')
 
 CHRN <- as.numeric(commandArgs(TRUE)[1])
 
 ### make directory
-system(paste0("mkdir ",output_path,"chr",CHRN))
+system(paste0("mkdir ",output_path,"/chr",CHRN))
+start_time<-Sys.time()
 
 ### chromosome number
 ## read info
 DB_info <- read.csv(file_DBsplit,header=TRUE)
-DB_info <- DB_info[DB_info$Chr==CHRN,]
+chr_splitnum <- sum(DB_info$Chr==CHRN)
+DB_info_chr <- DB_info[DB_info$Chr==CHRN,]
 
 ## open GDS
 genofile<-seqOpen(eval(parse(text = paste0("gds.chr",CHRN,".fn"))), readonly = FALSE)
@@ -26,36 +28,24 @@ ALT <- as.character(seqGetData(genofile, "$alt"))
 
 VarInfo_genome <- paste0(CHR,"-",position,"-",REF,"-",ALT)
 
-seqClose(genofile)
-
 ## Generate VarInfo
-for(kk in 1:dim(DB_info)[1])
+for(kk in 1:dim(DB_info_chr)[1])
 {
 	print(kk)
-	VarInfo <- VarInfo_genome[(position>=DB_info$Start_Pos[kk])&(position<=DB_info$End_Pos[kk])]
+	VarInfo <- VarInfo_genome[(position>=DB_info_chr$Start_Pos[kk])&(position<=DB_info_chr$End_Pos[kk])]
 	VarInfo <- data.frame(VarInfo)
-	write.csv(VarInfo,paste0(output_path,"chr",CHRN,"/VarInfo_chr",CHRN,"_",kk,".csv"),quote=FALSE,row.names = FALSE)
+	write.csv(VarInfo,paste0(output_path,"/chr",CHRN,"/VarInfo_chr",CHRN,"_",kk,".csv"),quote=FALSE,row.names = FALSE)
 }
-
-rm(list=ls())
 gc()
-
-### anno channel (subset)
-anno_colnum <- c(1,8,9:12,14,16,19,23,25:36)
-
-### chromosome number
-## annotate (seperate)
-DB_info <- read.csv(file_DBsplit,header=TRUE)
-chr_splitnum <- sum(DB_info$Chr==CHRN)
 
 for(kk in 1:chr_splitnum)
 {
 	print(kk)
-	system(paste0(xsv," join --left VarInfo ",output_path,"chr",CHRN,"/VarInfo_chr",CHRN,"_",kk,".csv variant_vcf ",DB_path,"/chr",CHRN,"_",kk,".csv > ",output_path,"chr",CHRN,"/Anno_chr",CHRN,"_",kk,".csv"))
+	system(paste0(xsv," join --left VarInfo ",output_path,"/chr",CHRN,"/VarInfo_chr",CHRN,"_",kk,".csv variant_vcf ",DB_path,"/chr",CHRN,"_",kk,".csv > ",output_path,"/chr",CHRN,"/Anno_chr",CHRN,"_",kk,".csv"))
 }
 
 ## merge info
-Anno <- paste0(output_path,"chr",CHRN,"/Anno_chr",CHRN,"_",seq(1:chr_splitnum),".csv ")
+Anno <- paste0(output_path,"/chr",CHRN,"/Anno_chr",CHRN,"_",seq(1:chr_splitnum),".csv ")
 merge_command <- paste0(xsv," cat rows ",Anno[1])
 
 for(kk in 2:chr_splitnum)
@@ -63,43 +53,23 @@ for(kk in 2:chr_splitnum)
 	merge_command <- paste0(merge_command,Anno[kk])
 }
 
-merge_command <- paste0(merge_command,"> ",output_path,"chr",CHRN,"/Anno_chr",CHRN,".csv")
-
+merge_command <- paste0(merge_command,"> ",output_path,"/chr",CHRN,"/Anno_chr",CHRN,".csv")
 system(merge_command)
 
-## subset
-anno_colnum_xsv <- c()
-for(kk in 1:(length(anno_colnum)-1))
-{
-	anno_colnum_xsv <- paste0(anno_colnum_xsv,anno_colnum[kk],",")
-}
-anno_colnum_xsv <- paste0(anno_colnum_xsv,anno_colnum[length(anno_colnum)])
-
-system(paste0(xsv," select ",anno_colnum_xsv," ",output_path,"chr",CHRN,"/Anno_chr",CHRN,".csv > ",output_path,"chr",CHRN,"/Anno_chr",CHRN,"_STAARpipeline.csv"))
-
-rm(list=ls())
 gc()
 ### read annotation data
-FunctionalAnnotation <- read_csv(paste0(dir_anno,"chr",CHRN,"/",anno_file_name_1,CHRN,anno_file_name_2),
-col_types=list(col_character(),col_double(),col_double(),col_double(),col_double(),
-				col_double(),col_double(),col_double(),col_double(),col_double(),
-				col_character(),col_character(),col_character(),col_double(),col_character(),
-				col_character(),col_character(),col_character(),col_character(),col_double(),
-				col_double(),col_character()))
+FunctionalAnnotation <- read_csv(paste0(output_path,"/chr",CHRN,"/Anno_chr",CHRN,".csv"))
 
 dim(FunctionalAnnotation)
 
-## rename colnames
-colnames(FunctionalAnnotation)[2] <- "apc_conservation"
-colnames(FunctionalAnnotation)[7] <- "apc_local_nucleotide_diversity"
 
 ## open GDS
-genofile<-seqOpen(eval(parse(text = paste0("gds.chr",CHRN,".fn"))), readonly = FALSE)
-
 Anno.folder <- addfolder.gdsn(index.gdsn(genofile, "annotation/info"), "FunctionalAnnotation")
 add.gdsn(Anno.folder, "FunctionalAnnotation", val=FunctionalAnnotation, compress="LZMA_ra", closezip=TRUE)
+genofile
 
 seqClose(genofile)
+end_time<-Sys.time()
 
-
-
+print("time:")
+end_time - start_time
--- Scripts/CSV/config.R
+++ Scripts/CSV/config.R
@@ -1,91 +1,85 @@
 ### DB split information 
-file_DBsplit <- "./FAVORdatabase_chrsplit.csv"
+file_DBsplit <- "../SQL/FAVORdatabase_chrsplit.csv"
+
 ### output
-output_path <- "/n/holyscratch01/xlin/xihao_zilin/TOPMed_F9/"
+output_path <- "../../../Output"
 
 ### xsv directory
-xsv <- "/n/home05/zilinli/.cargo/bin/xsv"
-
-### output (Step 2 and Step 3)
-output_path <- "/n/holyscratch01/xlin/xihao_zilin/TOPMed_F9/"
+xsv <- "~/.cargo/bin/xsv"
 
 ### DB file
-DB_path <- "/n/holyscratch01/xlin/xihao_zilin/FAVORAnnotatorDB/"
-
-### annotation file
-dir_anno <- "/n/holyscratch01/xlin/xihao_zilin/TOPMed_F9/"
-anno_file_name_1 <- "Anno_chr"
-anno_file_name_2 <- "_STAARpipeline.csv"
+DB_path <- "../../../FullDB/FAVORDB/"
 
 #---------chr1-----------------------
-vcf.chr1.fn<-"/n/location/input.vcf"
-gds.chr1.fn<-"/n/location/output.gds"
+vcf.chr1.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr1.vcf"
+gds.chr1.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr1.agds"
 #---------chr2-----------------------
-vcf.chr2.fn<-"/n/location/input.vcf"
-gds.chr2.fn<-"/n/location/output.gds"
+vcf.chr2.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr2.vcf"
+gds.chr2.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr2.agds"
 #---------chr3-----------------------
-vcf.chr3.fn<-"/n/location/input.vcf"
-gds.chr3.fn<-"/n/location/output.gds"
+vcf.chr3.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr3.vcf"
+gds.chr3.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr3.agds"
 #---------chr4-----------------------
-vcf.chr4.fn<-"/n/location/input.vcf"
-gds.chr4.fn<-"/n/location/output.gds"
+vcf.chr4.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr4.vcf"
+gds.chr4.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr4.agds"
 #---------chr5-----------------------
-vcf.chr5.fn<-"/n/location/input.vcf"
-gds.chr5.fn<-"/n/location/output.gds"
+vcf.chr5.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr5.vcf"
+gds.chr5.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr5.agds"
 #---------chr6-----------------------
-vcf.chr6.fn<-"/n/location/input.vcf"
-gds.chr6.fn<-"/n/location/output.gds"
+vcf.chr6.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr6.vcf"
+gds.chr6.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr6.agds"
 #---------chr7-----------------------
-vcf.chr7.fn<-"/n/location/input.vcf"
-gds.chr7.fn<-"/n/location/output.gds"
-#---------chr8-----------------------
-vcf.chr8.fn<-"/n/location/input.vcf"
-gds.chr8.fn<-"/n/location/output.gds"
-#---------chr9-----------------------
-vcf.chr9.fn<-"/n/location/input.vcf"
-gds.chr9.fn<-"/n/location/output.gds"
+vcf.chr7.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr7.vcf"
+gds.chr7.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr7.agds"
+#---------chr1-----------------------
+vcf.chr8.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr8.vcf"
+gds.chr8.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr8.agds"
+#---------chr1-----------------------
+vcf.chr9.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr9.vcf"
+gds.chr9.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr9.agds"
 #---------chr10-----------------------
-vcf.chr10.fn<-"/n/location/input.vcf"
-gds.chr10.fn<-"/n/location/output.gds"
+vcf.chr10.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr10.vcf"
+gds.chr10.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr10.agds"
+#---------chr10-----------------------
+vcf.chr10.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr10.vcf"
+gds.chr10.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr10.agds"
 #---------chr11-----------------------
-vcf.chr11.fn<-"/n/location/input.vcf"
-gds.chr11.fn<-"/n/location/output.gds"
+vcf.chr11.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr11.vcf"
+gds.chr11.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr11.agds"
+#---------chr11-----------------------
+vcf.chr11.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr11.vcf"
+gds.chr11.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr11.agds"
 #---------chr12-----------------------
-vcf.chr12.fn<-"/n/location/input.vcf"
-gds.chr12.fn<-"/n/location/output.gds"
+vcf.chr12.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr12.vcf"
+gds.chr12.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr12.agds"
 #---------chr13-----------------------
-vcf.chr13.fn<-"/n/location/input.vcf"
-gds.chr13.fn<-"/n/location/output.gds"
+vcf.chr13.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr13.vcf"
+gds.chr13.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr13.agds"
 #---------chr14-----------------------
-vcf.chr14.fn<-"/n/location/input.vcf"
-gds.chr14.fn<-"/n/location/output.gds"
+vcf.chr14.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr14.vcf"
+gds.chr14.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr14.agds"
 #---------chr15-----------------------
-vcf.chr15.fn<-"/n/location/input.vcf"
-gds.chr15.fn<-"/n/location/output.gds"
+vcf.chr15.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr15.vcf"
+gds.chr15.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr15.agds"
 #---------chr16-----------------------
-vcf.chr16.fn<-"/n/location/input.vcf"
-gds.chr16.fn<-"/n/location/output.gds"
+vcf.chr16.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr16.vcf"
+gds.chr16.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr16.agds"
 #---------chr17-----------------------
-vcf.chr17.fn<-"/n/location/input.vcf"
-gds.chr17.fn<-"/n/location/output.gds"
+vcf.chr17.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr17.vcf"
+gds.chr17.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr17.agds"
 #---------chr18-----------------------
-vcf.chr18.fn<-"/n/location/input.vcf"
-gds.chr18.fn<-"/n/location/output.gds"
+vcf.chr18.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr18.vcf"
+gds.chr18.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr18.agds"
 #---------chr19-----------------------
-vcf.chr19.fn<-"/n/location/input.vcf"
-gds.chr19.fn<-"/n/location/output.gds"
+vcf.chr19.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr19.vcf"
+gds.chr19.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr19.agds"
 #---------chr20-----------------------
-vcf.chr20.fn<-"/n/location/input.vcf"
-gds.chr20.fn<-"/n/location/output.gds"
+vcf.chr20.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr20.vcf"
+gds.chr20.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr20.agds"
 #---------chr21-----------------------
-vcf.chr21.fn<-"/n/location/input.vcf"
-gds.chr21.fn<-"/n/location/output.gds"
+vcf.chr21.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr21.vcf"
+gds.chr21.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr21.agds"
 #---------chr22-----------------------
-vcf.chr22.fn<-"/n/location/input.vcf"
-gds.chr22.fn<-"/n/location/output.gds"
-#---------chrX-----------------------
-vcf.chrX.fn<-"/n/location/input.vcf"
-gds.chrX.fn<-"/n/location/output.gds"
-#---------chrY-----------------------
-vcf.chrY.fn<-"/n/location/input.vcf"
-gds.chrY.fn<-"/n/location/output.gds"
+vcf.chr22.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr22.vcf"
+gds.chr22.fn<-"../../../Data/gnomad.genomes.v3.1.2.sites.chr22.agds"
+
