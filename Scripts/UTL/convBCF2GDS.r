90278c8d002ebb606b70d6985421077afabed5d8
Hufeng Zhou <zhouhufeng@boslogin03.rc.fas.harvard.edu>
2022-12-06 12:29:41 -0500

    VCF to GDS

--- Scripts/UTL/convBCF2GDS.r
+++ Scripts/UTL/convBCF2GDS.r
@@ -1,16 +1,17 @@
 #############################################################################
 #Title:    convertVCFtoGDS      
 #Function: 
-# * Build the GDS file from VCF files
+# * Build the GDS file from BCF files
 #Author:   Hufeng Zhou
 #Time:     Aug 27th 2022
+# This only runs on single core, therefore very slow.
 #############################################################################
 library(gdsfmt)
 library(SeqArray)
 
 vcf.fn=as.character(commandArgs(TRUE)[1])
 gds.fn=as.character(commandArgs(TRUE)[2])
-seqVCF2GDS(vcf.fn, gds.fn, parallel=10)
+seqBCF2GDS(vcf.fn, gds.fn, storage.option="LZMA_RA", bcftools="bcftools")
 genofile<-seqOpen(gds.fn, readonly = FALSE)
 print("GDS built")
 
